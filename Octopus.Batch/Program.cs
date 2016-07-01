d4b95ea91a2a88d6ee8a01fa6bb864a5bb8f3417
Fabrice Michellonet <fabrice.michellonet@seloger.com>
2016-07-01 15:59:06 +0200

    [ADD] Ajout du projet de batch

---
+++ Octopus.Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Octopus.Batch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello batch");
+        }
+    }
+}
